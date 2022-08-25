da132600272862a9d1dbf2da660b1004b798f99b
Pecze Dávid <peczedavid@gmail.com>
2022-08-25 13:45:16 +0200

    Hardcoded 2x2

--- OpenGL-Engine/src/Rendering/Geometry/Plane.cpp
+++ OpenGL-Engine/src/Rendering/Geometry/Plane.cpp
@@ -1,6 +1,6 @@
 #include "Rendering/Geometry/Plane.h"
 
-Plane::Plane(uint32_t width, uint32_t div)
+Plane::Plane(uint32_t width, uint32_t _div)
 {
 	glGenVertexArrays(1, &m_Vao);
 	glGenBuffers(1, &m_Vbo);
@@ -9,16 +9,39 @@ Plane::Plane(uint32_t width, uint32_t div)
 	glBindVertexArray(m_Vao);
 	glBindBuffer(GL_ARRAY_BUFFER, m_Vbo);
 
-	const float a = 0.5f;
-	float verticesSquare[4 * 5] = {
-		-a,  0,  a,  0, 0,    a,  0,  a, 1, 0,
-		 a,  0, -a,  1, 1,   -a,  0, -a, 0, 1
+	//  6---7---8
+	//  |   |   |
+	//  3---4---5
+	//  |   |   |
+	//  0---1---2
+
+	constexpr float w = 2.0f;
+	constexpr int div = 2;
+	float verticesSquare[(2 + 1) * (2 + 1) * (3 + 2)] = {
+		/*0*/ (float)0 / (float)div * w, 0, (float)0 / (float)div * w, (float)0 / div, (float)0 / div,
+		/*1*/ (float)1 / (float)div * w, 0, (float)0 / (float)div * w, (float)1 / div, (float)0 / div,
+		/*2*/ (float)2 / (float)div * w, 0, (float)0 / (float)div * w, (float)2 / div, (float)0 / div,
+
+		/*3*/ (float)0 / (float)div * w, 0, (float)1 / (float)div * w, (float)0 / div, (float)1 / div,
+		/*4*/ (float)1 / (float)div * w, 0, (float)1 / (float)div * w, (float)1 / div, (float)1 / div,
+		/*5*/ (float)2 / (float)div * w, 0, (float)1 / (float)div * w, (float)2 / div, (float)1 / div,
+
+		/*6*/ (float)0 / (float)div * w, 0, (float)2 / (float)div * w, (float)0 / div, (float)2 / div,
+		/*7*/ (float)1 / (float)div * w, 0, (float)2 / (float)div * w, (float)1 / div, (float)2 / div,
+		/*8*/ (float)2 / (float)div * w, 0, (float)2 / (float)div * w, (float)2 / div, (float)2 / div,
 	};
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesSquare), &verticesSquare[0], GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_Ebo);
-	uint32_t indicesSquare[4] = {
-		0, 1, 2, 3
+	uint32_t indicesSquare[div * div * 4] = {
+		// 0 * 3 + 0
+		0, 0 + 1, 0 + div + 1 + 1, 0 + div + 1,
+		// 0 * 3 + 1
+		1, 1 + 1, 1 + div + 1 + 1, 1 + div + 1,
+		// 1 * 3 + 0
+		3, 3 + 1, 3 + div + 1 + 1, 3 + div + 1,
+		// 1 * 3 + 1
+		4, 4 + 1, 4 + div + 1 + 1, 4 + div + 1
 	};
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesSquare), &indicesSquare[0], GL_STATIC_DRAW);
 
@@ -43,5 +66,5 @@ void Plane::GenerateMesh(uint32_t width, uint32_t div)
 void Plane::Render()
 {
 	glBindVertexArray(m_Vao);
-	glDrawElements(GL_PATCHES, 4, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_PATCHES, 16, GL_UNSIGNED_INT, 0);
 }
