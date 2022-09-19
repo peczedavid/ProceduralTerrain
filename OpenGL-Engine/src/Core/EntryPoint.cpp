7b57d3c0e20e7cadfa9fe240da392b6d0d9670ee
Pecze Dávid <peczedavid@gmail.com>
2022-09-19 09:49:51 +0200

    Better GPU selection

--- OpenGL-Engine/src/Core/EntryPoint.cpp
+++ OpenGL-Engine/src/Core/EntryPoint.cpp
@@ -1,6 +1,14 @@
 #include "Core/Application.h"
+#include <wtypes.h>
 
 Application* CreateApplication();
+
+constexpr int gpu = 0;
+
+extern "C" {
+	_declspec(dllexport) DWORD NvOptimusEnablement = static_cast<DWORD>(gpu);
+	_declspec(dllexport) int AmdPowerXpressRequestHighPerformance = gpu;
+}
 
 int main(int argc, char** argv)
 {
